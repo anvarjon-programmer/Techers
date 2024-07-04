c22d399e5b54d03a2e1a32b791133109b17463d5
Anvarjon <143145991+anvarjon-programmer@users.noreply.github.com>
2024-07-04 07:38:20 +0500

    last

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -12,7 +12,7 @@ const Main = () => {
       <div className="album py-5">
     <div className='container'>
       <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-        {articles.map(item =>(
+        {/* {articles.map(item =>(
         <div className="col" key={item.id}>
           <div className="card h-100 shadow-sm">
             <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
@@ -32,7 +32,7 @@ const Main = () => {
               </div>
           </div>
         </div>
-      ))}
+      ))} */}
       </div>
     </div>
   </div>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,6 +3,8 @@ import logo from '../../assets/img/ALogo.jpg'
 import { useDispatch, useSelector } from "react-redux"
 import { removeItem } from "../../helpers/persistance-storage";
 import { logoutUser } from "../../slice/auth";
+import { FaBars } from "react-icons/fa";
+import { useState } from "react";
 
 const Navbar = () => {
   const {loggedIn,user} = useSelector(state => state.auth);
@@ -13,12 +15,72 @@ const Navbar = () => {
     removeItem("token")
     navigate('/login')
    }
+
+   const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleMenuToggle = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
   return (
     <header>
-       <div className="w-full bg-black h-[80px]"></div>
+
+<nav className={`${isMenuOpen ? 'h-[50vh] bg-black fixed right-0 left-0  z-50' : 'h-[100px] bg-red-500'} w-full transation-all md:block md:w-auto`} >
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+          {/* <img  src={logo} className=" w-[175px]" alt="Flowbite Logo" /> */}
+          Logo
+        </Link>
+         <div className='flex items-center gap-8'>
+          <button
+            data-collapse-toggle="navbar-default"
+            type="button"
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg  hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            aria-controls="navbar-default"
+            aria-expanded={isMenuOpen}
+            onClick={handleMenuToggle}
+          >
+            <span className="sr-only">Open main menu</span>
+            {isMenuOpen ? (
+              'X'
+            ) : (
+              <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
+            </svg>
+            )
+          
+            }
+          </button>
+
+          
+         </div>
+      </div>
+         <div className="w-full h-full flex items-center justify-center mb-10 ">
+         {
+            isMenuOpen ? (
+              <ul>
+            <li>
+              <Link className="text-[20px] text-white" to='/'>Bosh Sahifa</Link>
+            </li>
+            <li>
+              <Link className="text-[20px] text-white" to='/'>Bosh Sahifa</Link>
+            </li>
+            <li>
+              <Link className="text-[20px] text-white" to='/'>Bosh Sahifa</Link>
+            </li>
+            <li>
+              <Link className="text-[20px] text-white" to='/'>Bosh Sahifa</Link>
+            </li>
+          </ul>
+            ) : ''
+          }
+         </div>
+    </nav>
+
+
         <div className="d-flex flex-column flex-md-row align-items-center pb-3 mb-4 border-bottom container pt-2">
         <Link to='/'>
-            <img src={logo} alt="" width={100} height={20}/>
+            {/* <img src={logo} alt="" width={100} height={20}/> */}
+            <h1>Home</h1>
         </Link>
       <nav className="d-inline-flex mt-2 mt-md-0 ms-md-auto">
         {loggedIn ? (
